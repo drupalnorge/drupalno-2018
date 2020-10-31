c2a998ab6d1a95b32f9571b799b14f8dd75af494
Truls S. Yggeseth <truls@signwo.com>
2020-10-31 18:02:43 +0100

    Composerify + cleanup the project and Upgraded Drupal 8.9.7 + pathauto 1.8 + ctools 3.4 + field_group 3.1

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,7 +1,8 @@
 <?php
 
 $settings['hash_salt'] = 'WrEUmGDEt0xEHaV0jPZPIwImzfappybKHnHRZUrpfLLoLXPh0LzqBRH8iHPL8YnsltqtnIyLcQ';
-$config_directories['sync'] = '../config/sync';
+$settings['config_sync_directory'] =  '../config/sync';
+$settings['file_private_path'] = 'sites/default/private';
 
 // Automatically generated include for settings managed by ddev.
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
