306ccf11531975c4b32a08faf6871f919dd70821
Truls S. Yggeseth <truls@signwo.com>
2021-03-13 14:07:47 +0100

    Update DDEV

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,14 +1,14 @@
 <?php
 
 $settings['hash_salt'] = 'WrEUmGDEt0xEHaV0jPZPIwImzfappybKHnHRZUrpfLLoLXPh0LzqBRH8iHPL8YnsltqtnIyLcQ';
+$settings['config_exclude_modules'] = ['devel', 'stage_file_proxy', 'config_exclude'];
 $config['config_split.config_split.development']['status'] = FALSE;
-$settings['config_sync_directory'] =  '../config/sync';
 $settings['file_private_path'] = 'sites/default/private';
+$settings['config_sync_directory'] =  '../config/sync';
 
 // Automatically generated include for settings managed by ddev.
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
-  $config['config_split.config_split.development']['status'] = TRUE;
 }
 
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
